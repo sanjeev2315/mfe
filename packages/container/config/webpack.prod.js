a71a3b39ae224fb04887904b46c1c6b0a757ef57
Sanjeev Kumar 12 <sankumar5@publicisgroupe.net>
2021-07-08 00:59:14 +0530

    added marketing micro front end

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packagejson = require('./../package.json');
 
-const domain = process.env.PRODUCTION_NAME
+const domain = process.env.PRODUCTION_DOMAIN
 
 const prodConfig = {
     mode: 'production',
